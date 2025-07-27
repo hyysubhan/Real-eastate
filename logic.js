db6c8effc68066f08fc9f5b5e77bef58b462c507
hyysubhan <subhanarha16@gmail.com>
2025-07-27 22:34:25 +0500

    full project is here

--- logic.js
+++ logic.js
@@ -1,3 +1,4 @@
+// mobile header
 let icon = document.querySelector('.mobileheadr');
 let menu = document.querySelector('.pages');
 let menuopen = false;
@@ -11,3 +12,26 @@ icon.addEventListener('click', () => {
         menuopen = false;
     }
 });
+// for slide herosection
+let currentslide = 0;
+let slides = document.querySelectorAll('.imgs .slide');
+const totalslides = slides.length;
+
+document.querySelector('.next').addEventListener('click', () => {
+    changeslide(currentslide + 1);
+    console.log("next is clicked");
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    changeslide(currentslide - 1);
+    console.log("prev is clicked");
+});
+
+function changeslide(index) {
+    slides[currentslide].classList.remove('active');
+    currentslide = (index + totalslides) % totalslides; // loop back
+    slides[currentslide].classList.add('active');
+}
+setInterval( () =>{
+      changeslide(currentslide + 1);
+}, 5000);
